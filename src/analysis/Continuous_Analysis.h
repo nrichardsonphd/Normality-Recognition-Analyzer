18c7af869bc443900b6b06ef66240fcebe6e2340
Dr. Nicholas Richardson <richardsonn@nsula.edu>
2018-07-12 16:24:25 -0500

    class analysis

--- src/analysis/Continuous_Analysis.h
+++ src/analysis/Continuous_Analysis.h
@@ -8,15 +8,41 @@ using namespace std;
 
 #include "Analyze_List.h"
 
-// Initialize continuous analaysis
-// This is called after the 1st sequence is started
-void Continuous_Analysis_Initial( unsigned long long int *initial_results, ostream &out );
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// This class is designed to be editted by the user.  Any variables needed can be stored here
+// as necessary.  The functions Initial, Interval, and Summary will be called when performing
+// continuous analysis on user determined intervals
+///////////////////////////////////////////////////////////////////////////////////////////////////
 
-// This is called based on granularity of full test. Every N Sequences are called here
-void Continuous_Analysis_Interval( unsigned long long int *interval_results, ostream &out );
+class Constant_Analysis
+{
+	public:
+		Constant_Analysis( unsigned int maximum_classes );
 
-// This is called after all calculations are completed with the final results
-void Continuous_Analysis_Summary(unsigned long long int *final_results, ostream &out);
 
+		void Set_Output_Stream( ostream &out );
+
+		// Initialize continuous analaysis
+		// This is called after the 1st sequence is started
+		void Continuous_Analysis_Initial( unsigned long long int *initial_results );
+
+		// This is called based on granularity of full test. Every N Sequences are called here
+		void Continuous_Analysis_Interval( unsigned long long int *interval_results);
+
+		// This is called after all calculations are completed with the final results
+		void Continuous_Analysis_Summary( unsigned long long int *final_results );
+
+	private:
+		Analyze_List al;
+		unsigned long long int max;
+		unsigned long long int min;
+
+		ostream out;
+
+		void Default_Initial( unsigned long long int *initial_results );
+		void Default_Interval( unsigned long long int *initial_results );
+		void Default_Summary( unsigned long long int *initial_results );
+
+};
 
 #endif
