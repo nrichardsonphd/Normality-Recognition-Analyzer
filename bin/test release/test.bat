9e3e1ba13995c14eeaf7054c63c80d30cdb1bb46
Dr. Nicholas Richardson <richardsonn@nsula.edu>
2018-07-19 11:57:20 -0500

    Create template batch file for running releases with multiple arguments

---
+++ bin/test release/test.bat
@@ -0,0 +1,7 @@
+title Normality Recognition Analyzer 0.2b Release Test
+	
+SET exe="Normality Recognition Analyzer - 0.2b release.exe"
+
+echo Testing
+
+%exe% -d 1000 -f pi1K.txt -o batch_output.txt
