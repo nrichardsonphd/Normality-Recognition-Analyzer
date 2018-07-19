336f98e04b11f4b5dd3ae8e0fb2045c1475cea0e
Dr. Nicholas Richardson <richardsonn@nsula.edu>
2018-07-19 14:44:11 -0500

    batch file with numbered files

--- bin/test release/test.bat
+++ bin/test release/test.bat
@@ -1,7 +1,13 @@
 title Normality Recognition Analyzer 0.2b Release Test
 	
 SET exe="Normality Recognition Analyzer - 0.2b release.exe"
-
+SET testfile=pi1K.txt
+SET digits=1000
+SET options="-r"
 echo Testing
 
-%exe% -d 1000 -f pi1K.txt -o batch_output.txt
+SET fileno=1
+
+%exe% -d %digits% %options% -f %testfile% -o "batch_output%fileno%.txt"
+Set /a fileno=%fileno%+1
+%exe% -d %digits% %options% -f %testfile% -o "batch_output%fileno%.txt"
