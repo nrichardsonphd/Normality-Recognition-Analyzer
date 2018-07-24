77ce2474f0836e8be70fbfdf56f9a72eaf8bf0d7
Dr. Nicholas Richardson <richardsonn@nsula.edu>
2018-07-24 09:52:45 -0500

    command line instructions formatted

--- README.txt
+++ README.txt
@@ -1,5 +1,3 @@
-Command Line Arguments
-
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 // Command Line Arguments
 // This is a rough setup, no combining arguments
@@ -7,25 +5,29 @@ Command Line Arguments
 // 
 // nra.exe -t -T -N 2 -V 2 -p -s 5 -b 8 -f ../../data/Pi1K-dec.txt -r tmp.out
 //
-// -t		Run all tests to ensure program is correct (quiet)
-// -T		same as -t except detailed
+// -t					Run all tests to ensure program is correct (quiet)
+// -T					same as -t except detailed
 //
-// -N #		Select numbered Next Sequence
-//			1. Digit blocks
-// -V #		Select numbered Sequence Value
-//			1. Number value
-//			2. 
-// -h		hexadecimal input file, convert to binary
+// -N #					Select numbered Next Sequence
+//						1. Digit blocks
+//							2.
 //
-// -d #		Number of digits to test
-// -r		remove predecimal
-// -b #		maximum size of each sequence
-// -c #		maximum number of classes
-// -s		stream digits with overlapping blocks (nonoverlapping is default)
-// 
+// -V #					Select numbered Sequence Value
+//							1. Number value
+//							2. 
+//
+// -h					hexadecimal input file, converted to binary on the fly
+// -d #					Number of digits to test ( binary or decimal )
+// -r					remove predecimal
+// -b #					size of each sequence
+// -s					stream digits with overlapping blocks (nonoverlapping is default)
+// -c #					number of classes ( default is 2^n (hex) or 10^n(dec) where n is size of sequence )
+//
+// -C #					Continuous Testing, # is for granularity how often to calculate results
+// -F					select full results for continuous testing, default is only important results
+//
 // -f <filename>		select input file for test
 // -o <filename>		select output file for test
 // -q <filename>		quiet summary, only digits and X^2, no class values
 //
-// -C #		Continuous Testing, # is for granularity
 ///////////////////////////////////////////////////////////////////////////////////////////////////
--- src/common/command.h
+++ src/common/command.h
@@ -2,8 +2,9 @@
 #define COMMAND_H
 
 #define MAX_SCREEN_CLASSES		16								// maximum number of classes that will be displayed on the screen, file output larger sets
-#define CONTINUOUS_LOG			"../../logs/continuous.txt"		// store larger output here during continuous output
 
+#define CONTINUOUS_LOG			"../../logs/continuous.txt"		// store larger output here during continuous output
+#define FULL_LOG				"../../logs/full.txt"
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 // Command Line Arguments
@@ -12,28 +13,31 @@
 // 
 // nra.exe -t -T -N 2 -V 2 -p -s 5 -b 8 -f ../../data/Pi1K-dec.txt -r tmp.out
 //
-// -t		Run all tests to ensure program is correct (quiet)
-// -T		same as -t except detailed
+// -t					Run all tests to ensure program is correct (quiet)
+// -T					same as -t except detailed
 //
-// -N #		Select numbered Next Sequence
-//			1. Digit blocks
-// -V #		Select numbered Sequence Value
-//			1. Number value
-//			2. 
-// -h		hexadecimal input file, convert to binary
+// -N #					Select numbered Next Sequence
+//						1. Digit blocks
+//							2.
 //
-// -d #		Number of digits to test
-// -r		remove predecimal
-// -b #		maximum size of each sequence
-// -c #		maximum number of classes
-// -s		stream digits with overlapping blocks (nonoverlapping is default)
-// 
+// -V #					Select numbered Sequence Value
+//							1. Number value
+//							2. 
+//
+// -h					hexadecimal input file, converted to binary on the fly
+// -d #					Number of digits to test ( binary or decimal )
+// -r					remove predecimal
+// -b #					size of each sequence
+// -s					stream digits with overlapping blocks (nonoverlapping is default)
+// -c #					number of classes ( default is 2^n (hex) or 10^n(dec) where n is size of sequence )
+//
+// -C #					Continuous Testing, # is for granularity how often to calculate results
+// -F					select full results for continuous testing, default is only important results
+//
 // -f <filename>		select input file for test
 // -o <filename>		select output file for test
 // -q <filename>		quiet summary, only digits and X^2, no class values
 //
-// -C #		Continuous Testing, # is for granularity
-// -F		select full results for continuous testing, default is only important results
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 
 #include <iostream>
